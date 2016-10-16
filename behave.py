2411c9ff42714d71b3180bc84a6404e88f58d67e
danray <dray@healthwise.org>
2016-10-16 08:12:08 -0600

    experiment

--- behave.py
+++ behave.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 '''
- === BEHAVE ===
 
+ === BEHAVE ===
 Oh do behave!
 
 '''
@@ -26,30 +26,30 @@ def loop(robot):
     robot.say_text("oh").wait_for_completed()
 
     while do_the_loopy:
-        print("looping ...")
+        #print("looping ...")
         #face_to_follow = robot.world.wait_for_observed_face(timeout=30)
         #robot.say_text("Daniel... You are my friend!").wait_for_completed()
         turn_action = None
         if face_to_follow:
             # start turning towards the face
-            print("turn action")
+            if(face_to_follow.is_visible):
+                print(face_to_follow)
             #turn_action = robot.turn_towards_face(face_to_follow)
-            robot.say_text("peekaboo").wait_for_completed()
+            #robot.say_text("peekaboo").wait_for_completed()
 
         if not (face_to_follow and face_to_follow.is_visible):
             # find a visible face, timeout if nothing found after a short while
             try:
+                print("looking ...")
                 face_to_follow = robot.world.wait_for_observed_face(timeout=3)
                 if face_to_follow:
+                    #print("face_to_follow=" + str(face_to_follow))
                     robot.say_text("Daniel... You are my friend!").wait_for_completed()
-                    print("Daniel... You are my friend!")
-
             except asyncio.TimeoutError:
                 print("Didn't find a face!")
                 robot.say_text("Where everybody go?", voice_pitch = 1.0, duration_scalar=0.7).wait_for_completed()
                 #do_the_loopy = False
                 #return
-
             except KeyboardInterrupt:
                 print("")
                 print("Exit requested by user")
@@ -58,15 +58,16 @@ def loop(robot):
 
             if turn_action:
                 # Complete the turn action if one was in progress
+                print("starting turn_action.wait_for_completed()")
                 turn_action.wait_for_completed()
-                print("waited_for_completed")
+                print("finished turn_action.wait_for_completed()")
 
-            if do_the_loopy:
-                print("sleeping ...")
-                time.sleep(.3)
-            else:
-                print("done now, goodbye")
-        #pass
+        if do_the_loopy:
+            #print("sleeping ...")
+            time.sleep(.1)
+        else:
+            print("done now, goodbye")
+            #pass
 
 
 if __name__ == '__main__':
