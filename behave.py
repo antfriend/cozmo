0e60fe6b51c30e6d93a2c4c2fd1637ce2541d94d
danray <dray@healthwise.org>
2016-10-23 18:39:22 -0600

    doing stuff

--- behave.py
+++ behave.py
@@ -16,13 +16,14 @@ def run(sdk_conn):
     '''init'''
     robot = sdk_conn.wait_for_robot()
     robot.move_lift(-3)
-    robot.set_head_angle(degrees(20.0)).wait_for_completed()
+    robot.set_head_angle(degrees(80.0)).wait_for_completed()
+    waitForACubetap(robot)
     loop(robot)
 
-def loop(robot):
+def waitForACubetap(robot):
+    print("looking for a cube ...")
     cubes = None
-    print("doing")
-    #look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+    robot.set_head_angle(degrees(0.0)).wait_for_completed()
     cubes = robot.world.wait_until_observe_num_objects(num=1, object_type=cozmo.objects.LightCube, timeout=60)
     cubes[0].set_lights(cozmo.lights.green_light)
     #look_around.stop()
@@ -35,19 +36,29 @@ def loop(robot):
             cubes[0].set_lights(cozmo.lights.red_light)
             waiting_for_a_tap = False
             continue
-        except error as e:
-            print("waiting for a tap error: %s" % e)
-            #sys.exit("A connection error occurred: %s" % e)
-
-
-
-
+        except Exception as e:
+            print("error waiting for a tap: %s" % e)
+            pass
     print("TAP")
 
+def useThisFace(face_to_follow):
+    #print(face_to_follow)
+    nice_pose = face_to_follow.pose
+    #print(nice_pose)
+    posey = nice_pose.position.y
+    print(posey)
+    if(posey < -15.0):
+        #print("dude!")
+        return posey
+    return posey
+
+def loop(robot):
+    #look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+    cozmoHeadAngle = 45.0
     face_to_follow = None
-    '''The main loop'''
     do_the_loopy = True
-    robot.say_text("oh").wait_for_completed()
+    robot.set_head_angle(degrees(cozmoHeadAngle)).wait_for_completed()
+    #robot.say_text("oh").wait_for_completed()
 
     while do_the_loopy:
         #print("looping ...")
@@ -57,21 +68,32 @@ def loop(robot):
         if face_to_follow:
             # start turning towards the face
             if(face_to_follow.is_visible):
-                print(face_to_follow)
-            #turn_action = robot.turn_towards_face(face_to_follow)
-            #robot.say_text("peekaboo").wait_for_completed()
+                theY = useThisFace(face_to_follow)
+                #robot.set_head_angle(degrees(45.0)).wait_for_completed()
 
+                if(theY < 50.0):
+                    print("look up ...")
+                    cozmoHeadAngle = cozmoHeadAngle + 5
+                    robot.set_head_angle(degrees(cozmoHeadAngle)).wait_for_completed()
+                else:
+                    print("look down ...")
+                    cozmoHeadAngle = cozmoHeadAngle - 5
+                    robot.set_head_angle(degrees(cozmoHeadAngle)).wait_for_completed()
+                robot.turn_towards_face(face_to_follow).wait_for_completed()
         if not (face_to_follow and face_to_follow.is_visible):
             # find a visible face, timeout if nothing found after a short while
             try:
                 print("looking ...")
-                face_to_follow = robot.world.wait_for_observed_face(timeout=3)
+                face_to_follow = robot.world.wait_for_observed_face(timeout=10)
                 if face_to_follow:
                     #print("face_to_follow=" + str(face_to_follow))
-                    robot.say_text("Daniel... You are my friend!").wait_for_completed()
+                    theY = useThisFace(face_to_follow)
+                    robot.turn_towards_face(face_to_follow).wait_for_completed()
+                    #robot.say_text("Dan!").wait_for_completed()
             except asyncio.TimeoutError:
                 print("Didn't find a face!")
                 robot.say_text("Where everybody go?", voice_pitch = 1.0, duration_scalar=0.7).wait_for_completed()
+                robot.set_head_angle(degrees(45.0)).wait_for_completed()
                 #do_the_loopy = False
                 #return
             except KeyboardInterrupt:
@@ -98,6 +120,7 @@ if __name__ == '__main__':
     cozmo.setup_basic_logging()
 
     try:
-        cozmo.connect(run)
+        cozmo.run.connect_with_tkviewer(run)
+        #cozmo.connect(run)
     except cozmo.ConnectionError as e:
         sys.exit("A connection error occurred: %s" % e)
